b3cb712a9e0ed774ea7ba2ceb8d56ac39fd00c24
su8lim3kid <57498292+su8lim3kid@users.noreply.github.com>
2019-11-30 20:38:39 -0700

    second commit

--- flashcard/index.js
+++ flashcard/index.js
@@ -1,9 +1,34 @@
-var box = document.querySelector('.thecard');
+var card = document.getElementById('card');
 
-box.addEventListener("click",function(){
-    if (box.classList.contains('active')){
-        box.classList.remove('active');
-    } else{
-        box.classList.add('active')
-    }
+card.addEventListener('dblclick', function() {
+	if (!this.classList.contains('on')) {
+		this.classList.remove('off');
+		this.classList.add('on');
+	} else {
+		this.classList.remove('on');
+		this.classList.add('off');
+	}
+});
+var card = document.getElementById('card2');
+
+card.addEventListener('dblclick', function() {
+	if (!this.classList.contains('on')) {
+		this.classList.remove('off');
+		this.classList.add('on');
+	} else {
+		this.classList.remove('on');
+		this.classList.add('off');
+	}
+});
+
+var card = document.getElementById('card3');
+
+card.addEventListener('dblclick', function() {
+	if (!this.classList.contains('on')) {
+		this.classList.remove('off');
+		this.classList.add('on');
+	} else {
+		this.classList.remove('on');
+		this.classList.add('off');
+	}
 });
